6c5ef8e04f4518ee2ccd713a50abd7b906bbda9d
kevin schuchard <schuchard.kevin@gmail.com>
2019-11-26 10:23:02 -0700

    add shared, graph

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraphComponent } from './graph/graph.component';
+
+@NgModule({
+  declarations: [GraphComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
