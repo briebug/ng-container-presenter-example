09e51d5bbbb09bf0c91411f2153e90598688f9dc
kevin schuchard <schuchard.kevin@gmail.com>
2019-11-27 22:02:25 -0700

    rename

--- src/app/core/package.service.ts
+++ src/app/core/package.service.ts
@@ -14,18 +14,18 @@ export const enum ChartColors {
 export class PackageService {
   labels = ['2016', '2017', '2018', '2019'];
   randomNumber = (min = 1000, max = 2500) => Math.random() * (max - min) + min;
-  dataLabels = (data: number[]) =>
+  dataLabels$ = (data: number[]) =>
     of({ labels: this.labels, data }).pipe(delay(this.randomNumber()));
 
   get shipped$() {
-    return this.dataLabels([1000, 2000, 2450, 3200]);
+    return this.dataLabels$([1000, 2000, 2450, 3200]);
   }
 
   get returned$() {
-    return this.dataLabels([850, 700, 1200, 1700]);
+    return this.dataLabels$([850, 700, 1200, 1700]);
   }
 
   get rushed$() {
-    return this.dataLabels([600, 1200, 1300, 2400]);
+    return this.dataLabels$([600, 1200, 1300, 2400]);
   }
 }
