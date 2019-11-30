80c12ea2defce3349943d1025f37dc69860ced96
kevin schuchard <schuchard.kevin@gmail.com>
2019-11-29 21:08:33 -0700

    simplify

--- src/app/+other-dashboard/other-dashboard.component.ts
+++ src/app/+other-dashboard/other-dashboard.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-other-dashboard',
   templateUrl: './other-dashboard.component.html',
   styleUrls: ['./other-dashboard.component.scss'],
 })
-export class OtherDashboardComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class OtherDashboardComponent {}
